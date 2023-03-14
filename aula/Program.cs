804e1a05cc897c4077f5bf1aaf2211c45893333b
Breno Miguel <100931830+brenomiguel@users.noreply.github.com>
2023-03-14 11:03:31 -0300

    Add files via upload

---
+++ aula/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace aula;
+class Program
+{
+    static void Main(string[] args)
+    {
+        ExercicioAluno.Alunos aluno1 = new ExercicioAluno.Alunos();
+        aluno1.nome = "Fulçano da silva rolim";
+        aluno1.nota1 = 4.3;
+        aluno1.nota2 = 7.8;
+        aluno1.mensagem();
+    }
+}
